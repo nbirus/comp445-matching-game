7c1e95a608bbba5247b7f64b84dd08918c05cf52
Nathan Birus <nbirus@HARDING.EDU>
2016-11-30 20:47:17 -0600

    reinit

--- Ligths Out/Properties/AssemblyInfo.cs
+++ Ligths Out/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MatchingGame")]
+[assembly: AssemblyTitle("Ligths Out")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MatchingGame")]
+[assembly: AssemblyProduct("Ligths Out")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ Ligths Out/SecondPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Ligths_Out
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondPage : Page
+    {
+        public SecondPage()
+        {
+            this.InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var navManager = SystemNavigationManager.GetForCurrentView();
+            if (this.Frame.CanGoBack)
+            {
+                // Show Back button in title bar
+                navManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+            }
+            else
+            {
+                // Remove Back button from title bar
+                navManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
+            }
+
+            // Register BackRequested handler
+            navManager.BackRequested += SecondPage_BackRequested;
+        }
+
+        private void SecondPage_BackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (this.Frame.CanGoBack)
+                this.Frame.GoBack();
+        }
+    }
+}
